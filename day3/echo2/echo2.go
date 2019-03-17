8481d9aa6e8baede6693ba49187d46d322b6e611
hyc <hyc554@outlook.com>
2019-03-17 18:12:44 +0800

    1.1

--- day3/echo2/echo2.go
+++ day3/echo2/echo2.go
@@ -3,13 +3,26 @@ package main
 import (
 	"fmt"
 	"os"
+	// "strings"
 )
 
 func main() {
-	s,sep := "",""
-	for _, arg := range os.Args[1:]{
-		s += sep + arg
-		sep = " "
+	// s,sep := "",""
+	// for _, arg := range os.Args[1:]{
+	// 	s += sep + arg
+	// 	sep = " "
+	// }
+	// fmt.Println(s)
+	// 法2
+	// fmt.Println(strings.Join(os.Args[1:]," "))
+	// 练习1.1
+	// fmt.Println(os.Args[0])
+	// 练习1.2
+	// s,sep := "",""
+	for n, arg := range os.Args[1:]{
+		fmt.Println(n,arg)
+		
 	}
-	fmt.Println(s)
+	
+
 }
